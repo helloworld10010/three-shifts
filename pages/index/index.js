10f263fe1ddf869f3eb6de953ffde253a7fcd00f
piaoyao <guojam@outlook.com>
2022-05-21 16:47:47 +0800

    优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,25 +1,35 @@
 // index.js
-var arr = {"0":"白班","1":"夜班","2":"休息"};
+var arr = ["白班","夜班","休息"];
 function getDaysBetween(dateString1,dateString2){
-  var  startDate = Date.parse(dateString1);
-  var  endDate = Date.parse(dateString2);
-  if (startDate>endDate){
+  var  startDateMS = Date.parse(dateString1);
+  var  endDateMS = Date.parse(dateString2);
+  if (startDateMS > endDateMS){
+    wx.showToast({
+      title: '不支持向前查询',
+      duration:2000
+    })
+    return 0;
+  }
+  if (startDateMS==endDateMS){
       return 0;
   }
-  if (startDate==endDate){
-      return 0;
+  var startDate = new Date(startDateMS);
+  var endDate = new Date(endDateMS);
+  var days=(endDateMS - startDateMS)/(1*24*60*60*1000);
+  if(days < 1 && startDate.getDate() != endDate.getDate()){
+    return 1;
   }
-  var days=(endDate - startDate)/(1*24*60*60*1000);
-  return  days;
+  return days;
 }
 
 Page({
   data:{
-    current:"白班"
+    current:"第一次使用请先修正"
   },
 
-  onLoad:function(){
-   this.init();
+  onShow:function (){
+    console.log("onShow.........................................")
+    this.init()
   },
 
   actionFix:function(){
@@ -28,10 +38,14 @@ Page({
       itemList: ['白班', '夜班', '休息'],
       success (res) {
         console.log(res.tapIndex);
-        console.log("new date:"+new Date());
+        var newDate = new Date();
+        newDate.setHours(0)
+        newDate.setMinutes(0)
+        newDate.setSeconds(0)
+        newDate.setMilliseconds(0)
+        console.log("new date:"+ newDate);
         wx.setStorageSync('anchorDate', new Date());
         wx.setStorageSync('anchor', res.tapIndex);
-        wx.setStorageSync('test', "test data");
         page.init();
       },
       fail (res) {
@@ -46,20 +60,21 @@ Page({
   },
 
   init:function(){
+    console.log("init start....................")
     try{
       var anchorDate = wx.getStorageSync('anchorDate');
       var anchor = wx.getStorageSync('anchor');
       var testData = wx.getStorageSync('test');
-      console.log("testData:"+testData);
       console.log("anchorDate:"+anchorDate);
       console.log("anchor:"+anchor);
       if(anchorDate){
         var anchorDate = new Date(anchorDate);
         var now = new Date();
         var diff = getDaysBetween(anchorDate,now);
+        console.log("时间差为："+diff)
         console.log("current:"+(diff+anchor)%3);
         this.setData({
-          current:arr[(diff+anchor)%3]
+          current:arr[Math.trunc((diff+anchor)%3)]
         })
       } else {
         this.setData({
@@ -67,6 +82,7 @@ Page({
         })
       }
     }catch(e){
+      console.log(e)
       wx.showToast({
         title: '未知错误',
         icon:'error',
